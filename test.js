5438bc7fb4529406880bcd964da5191528d8158c
Marcin Niesluchowski <m.niesluchow@samsung.com>
2017-11-29 14:07:58 +0100

    Fix errors when running tests
    Sometimes JavaScript codebase fails due to timeout issue. Increase timeout
    as pointed [1] to fix that issue.
    
    [1] https://github.com/rstacruz/mocha-standard#timeouts
    
    Signed-off-by: Marcin Niesluchowski <m.niesluchow@samsung.com>

--- test.js
+++ test.js
@@ -141,5 +141,7 @@ describe('shared-git-hooks', () => {
 })
 
 describe('JavaScript codebase', function () {
+  this.timeout(10000)
+
   it('conforms to javascript standard style (http://standardjs.com)', require('mocha-standard'))
 })
